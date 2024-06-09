0b4f9b2a3ec9f44198b713b01382fd295161ca70
flashcy <cksdud7890@naver.com>
2024-06-09 23:16:04 +0900

    The project ended up.

--- src/routes/TerminalHome.jsx
+++ src/routes/TerminalHome.jsx
@@ -3,6 +3,7 @@ import route_image from "../assets/BusRoutes.svg"
 import bus_image from "../assets/BusImg.png"
 import arrow_right from "../assets/arrow_right.svg"
 import arrow_left from "../assets/arrow_left.svg"
+import { useEffect, useState } from "react";
 
 const Background = styled.div`
     background-image: linear-gradient(90deg, #112e59 0, #2563bf 60%);
@@ -28,13 +29,24 @@ const StationList = styled.div`
     align-items: start;
     height: 460px;
     padding-left: 20px;
-    & > p {
-        margin: 0;
-        font-size: 24px;
-    }
     & > p:nth-child(3) {
         font-size: 28px;
         font-weight: 500;
+    }
+`;
+const OuterP = styled.p`
+    margin: 0;
+    font-size: 24px;
+    position: relative;
+    & > strong {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        color: #cf4040;
+        font-size: 18px;
+        font-weight: 500;
+        width: 200px;
+        margin: 0;
     }
 `;
 const RightGroup = styled.div`
@@ -83,14 +95,15 @@ const BusNumber = styled.div`
 const GetInBtn = styled.div`
     width: 100px;
     height: 50px;
-    border: solid 3px #FFAD62;
+    border: solid 3px ${props => props.$primary ? '#FFAD62' : '#777'};
     border-radius: 10px;
     background-color: rgba(35, 37, 39, .7);
     margin: 15px;
+    cursor: pointer;
     & > p {
         font-size: 28px;
         font-weight: 500;
-        color: #FFAD62;
+        color: ${props => props.$primary ? '#FFAD62' : '#777'};
         margin: 0;
         text-align: center;
         line-height: 50px;
@@ -150,47 +163,167 @@ const Distance = styled.div`
 `;
 
 function TerminalHome() {
+    const url = 'http://www.devhsb.com:28900/busDeviceInfo?bus_id=';
+    const [userOnboardInfo, setUserOnboardInfo] = useState({});
+    const [busPosInfo, setBusPosInfo] = useState({});
+    const [routeList, setRouteList] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [busId, setBusId] = useState(1);
+    const [isUserWaiting, setIsUserWaiting] = useState(false);
+
+    const [stationArray, setStationArray] = useState(['-', '-', '-', '-', '-']);
+    const [onBoardArray, setOnBoardArray] = useState([0, 0, 0, 0, 0]);
+    let data;
+
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setCurrentDateTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        const result = window.prompt('버스 id 입력');
+        setBusId(result);
+        fetchSSE(result);
+    }, [])
+
+    useEffect(() => {
+        if(isLoading) return;
+        const idx = busPosInfo.now_station_no;
+        let userArr = [0, 0, 0, 0, 0];
+        let tmpArr = ['-', '-', '-', '-', '-'];
+        let j = 0;
+        for(let i = idx - 2; i <= idx + 2; i++) {
+            if (i < 0 || i >= routeList.length) continue;
+            if (userOnboardInfo.user_onboard_station_id == i) {
+                userArr[j] = 1;
+            }
+            tmpArr[j++] = routeList[i].station_name;
+            setStationArray(tmpArr);
+            setOnBoardArray(userArr);
+        }
+    }, [busPosInfo]);
+
+    const fetchSSE = async (id) => {
+        const eventSource = new EventSource(url + id);
+
+        eventSource.onopen = () => {
+            // 연결 직후
+        };
+        eventSource.onmessage = async (e) => {
+            const res = await e.data;
+            const parseData = JSON.parse(res);
+            data = parseData;
+            if (data.user_onboard_info == null || data.user_onboard_info == undefined)
+                setIsUserWaiting(false);
+            else {
+                setIsUserWaiting(true);
+            }
+            setUserOnboardInfo(data.user_onboard_info)
+            setBusPosInfo(data.bus_pos_info)
+            setRouteList(data.route_list)
+            setIsLoading(false);
+            //
+        };
+
+        eventSource.onerror = (e) => {
+            eventSource.close();
+        }
+    };
+
+    const getYearMonthDateString = () => {
+        const year = currentDateTime.getFullYear();
+        const month = currentDateTime.getMonth() + 1;
+        const today = currentDateTime.getDate();
+
+        return `${year}.${month < 10 ? '0' + month : month}.${today < 10 ? '0' + today : today}`;
+    }
+
+    const getDayString = () => {
+        const day = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+        return day[currentDateTime.getDay()];
+    }
+
+    const getTimeString = () => {
+        return `${currentDateTime.getHours()}:${currentDateTime.getMinutes()}:${currentDateTime.getSeconds()}`;
+    }
+
+    const handleOnClick = async () => {
+        if (userOnboardInfo == null || userOnboardInfo == undefined) return;
+        console.log(userOnboardInfo.service_id);
+        try {
+            const btnUrl = "http://www.hsbdev.com:28900/onBoardPassenger";
+            const response = await fetch(btnUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'service_id': userOnboardInfo.service_id})
+            })
+            const data = await response.json();
+            console.log(data);
+            if (data?.status === "Y" && data?.code === 200) {
+                console.log("The user got on the bus.")
+            } else {
+                console.log("POST method failed: status == N")
+            }
+        }
+        catch {
+            console.log("POST method failed: post failed");
+        }
+    }
+
     return (
+        <>
+        {!isLoading && (
         <Background>
-            <RouteContainer>
+            <RouteContainer>  
                 <img src={route_image} style={{width: '40px'}}/>
                 <StationList>
-                    <p>양곡도매시장</p>
-                    <p>교대역</p>
-                    <p>양곡도매시장</p>
-                    <p>양곡도매시장</p>
-                    <p>양곡도매시장</p>
+                    <OuterP>{stationArray[4]}<strong>{onBoardArray[4] === 1 && '시각장애인 승차'}</strong></OuterP>
+                    <OuterP>{stationArray[3]}<strong>{onBoardArray[3] === 1 && '시각장애인 승차'}</strong></OuterP>
+                    <OuterP>{stationArray[2]}<strong>{onBoardArray[2] === 1 && '시각장애인 승차'}</strong></OuterP>
+                    <OuterP>{stationArray[1]}</OuterP>
+                    <OuterP>{stationArray[0]}</OuterP>
                 </StationList>
             </RouteContainer>
             <RightGroup>
-                <CurrentStation>양곡도매시장</CurrentStation>
+                <CurrentStation>{routeList[busPosInfo.now_station_no].station_name}</CurrentStation>
                 <BusImage>
                     <img src={bus_image} />
                     <BusNumber>
                         <p>차량번호</p>
-                        <p>511</p>
+                        <p>{busId == 1 ? '440' : '333'}</p>
                     </BusNumber>
                 </BusImage>
-                <GetInBtn>
+                <GetInBtn onClick={handleOnClick} $primary={isUserWaiting}>
                     <p>승차</p>
                 </GetInBtn>
             </RightGroup>
             <TimeViewer>
                 <DateViewer>
-                    <p>2024.06.08</p>
-                    <p>토요일</p>
+                    <p>{getYearMonthDateString()}</p>
+                    <p>{getDayString()}</p>
                 </DateViewer>
-                <p>18:05:46</p>
+                <p>{getTimeString()}</p>
             </TimeViewer>
+            {userOnboardInfo && (
             <BottomLeftItems>
                 <img src={arrow_right} style={{width: '80px'}}/>
-                <FrontOrBack>후방</FrontOrBack>
+                <FrontOrBack>{busPosInfo.is_user_infront == 1 ? '전방' : '후방'}</FrontOrBack>
                 <Distance>
-                    <p>1.7</p>
-                    <p>m</p>
+                    <p>{busPosInfo.user_bus_dist >= 100 ? Math.floor(busPosInfo.user_bus_dist) / 100 : Math.floor(busPosInfo.user_bus_dist)}</p>
+                    <p>{busPosInfo.user_bus_dist >= 100 ? 'm' : 'cm'}</p>
                 </Distance>
             </BottomLeftItems>
+            )}
         </Background>
+        )}
+        </>
     );
 }
 
